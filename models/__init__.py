365c4d91546f51b92f1981b97f87a604c0b78ac2
kelvin defrNBAH <kevinocholah@gmail.com>
2024-07-12 12:36:14 +0300

    web_dynamic

--- models/__init__.py
+++ models/__init__.py
@@ -2,22 +2,15 @@
 
 from os import getenv
 
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 """import the file_storage engine and db storage class from the models engine directory"""
+
 if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
     storage.reload() 
 
-
-"""___init__ magic method for models directory"""
-from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
-storage.reload()
- """ in storage object and reload the data from the file storage instanciates the file storage from the file storage class if the db is set to db_ storage and else the  file storage si ds ewr set to none """
 
 
